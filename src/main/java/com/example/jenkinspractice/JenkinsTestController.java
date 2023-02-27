cf35158876729d4e429cebc9f960f810a4cbf179
gayoung.yang.0 <gayoung.yang.0@gmail.com>
2023-02-27 13:07:56 +0900

    asdf

---
+++ src/main/java/com/example/jenkinspractice/JenkinsTestController.java
@@ -0,0 +1,14 @@
+package com.example.jenkinspractice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsTestController {
+    @GetMapping("/helloworld")
+    public ResponseEntity<?> helloWorld() {
+        return new ResponseEntity<String>("Hello World", HttpStatus.OK);
+    }
+}
